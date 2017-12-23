0d522d84ca6afc007f3f93ac8ae205a8fccf2296
Kivanc Erten <kerten@ebay.com>
2017-12-23 22:21:08 +0300

    usdt_btc 1.25

--- run.php
+++ run.php
@@ -1,8 +1,8 @@
 <?php
 namespace TradingBot;
 
-define('BUY_PERCENT_LIMIT', 0.25);
-define('SELL_PERCENT_LIMIT', 0.25);
+define('BUY_PERCENT_LIMIT', 1.25);
+define('SELL_PERCENT_LIMIT', 1.25);
 define('DRY_RUN', false);
 
 include_once 'secrets.php';
@@ -63,7 +63,7 @@ exit;
 */
 
 
-$tradingBot = new TradingBot($tradingClient, 'BTC_LTC');
+$tradingBot = new TradingBot($tradingClient, 'USDT_BTC');
 
 echo 'Date: ' . date('c') . PHP_EOL;
 
